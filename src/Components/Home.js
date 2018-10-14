428ee188dc3416edc29c3f81371ec42503b3d486
Will Williams <will.wms@gmail.com>
2018-10-14 16:07:04 -0700

    feat: Abstract static template HTML into new BookCase component, then import through Home.js into App.js

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BookCase from './BookCase'
+// import AddBook from './AddBook'
+// import Title from './Title'
+
+const Home = (props) => {
+  return (
+    <div className="main-page">
+{/*      <Title />*/}
+      <BookCase />
+{/*      <AddBook />*/}
+    </div>
+  )
+}
+
+export default Home
